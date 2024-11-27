79c7e8c069c30a7279ee57cc7dc6aee5ca31c519
tallerespa <tallerespa@PAL_LAB40303>
2024-11-27 19:05:42 -0300

    Agregar archivos de proyecto.

---
+++ AppDeveloperDB.AppMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppDeveloperDB.AppMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppDeveloperDB.AppMobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppDeveloperDB.AppMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
